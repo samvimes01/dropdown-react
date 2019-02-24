938786f8ff329fb427fa577bc285f3c0f88b5a1a
SamVimes0 <k.aleksandr88@gmail.com>
2019-02-24 20:43:08 +0200

    Updates

---
+++ precache-manifest.5f4d37a447f32fa8e04fd4ed6c19de54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ceb5f1d2a62c54e91fef",
+    "url": "/dropdown-react/static/css/main.8594cb2d.chunk.css"
+  },
+  {
+    "revision": "ceb5f1d2a62c54e91fef",
+    "url": "/dropdown-react/static/js/main.ceb5f1d2.chunk.js"
+  },
+  {
+    "revision": "589bbf103b8039203362",
+    "url": "/dropdown-react/static/js/runtime~main.589bbf10.js"
+  },
+  {
+    "revision": "4b1ad3dac61b8023da81",
+    "url": "/dropdown-react/static/js/2.4b1ad3da.chunk.js"
+  },
+  {
+    "revision": "e18d9e24eb7ebd267ea4f40651d6cc1c",
+    "url": "/dropdown-react/index.html"
+  }
+];
